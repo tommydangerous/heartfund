4fc42ad5b96f0c0f32caf4d132919a8ad04003d7
QuantumVenturess <quantumventuress@gmail.com>
2012-12-06 10:59:46 -0800

    finished writing short info, prologue, nightmare, aftermath, epilogue

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,65 +2,143 @@ module PagesHelper
 
 	def short_info
 		a = content_tag :p do
-			"Halo 4 is a first-person shooter video game developed by 343 Industries for the Xbox 360 console. It is the first installment in a new trilogy of Halo games, named the Reclaimer Trilogy."
+			"Novels cannot describe the joy that Laurie's life brings to this 
+			world. Paintings and songs cannot capture the beauty and 
+			compassion she possesses. No force can match the impact Laurie 
+			has made on the lives of those around her. She is the most 
+			wonderful, kind, compassionate, and caring person."
 		end
 		b = content_tag :p do
-			"The game was released worldwide on November 6, 2012, with the exception of Japan where the game was released on November 8, 2012. Halo 4's story begins four years after the ending of Halo 3; the player assumes the role of the Master Chief, a cybernetically enhanced supersoldier."
+			"The nightmare started one dreadful evening on October 18, 2012. 
+			The Sun was low and the dust was settling as Laurie began to 
+			climb. Midway through her ascension, she realized the summit was 
+			out of reach and retreat was the only option available."
 		end
 		c = content_tag :p do
-			"The story is mainly set on a Forerunner planet, where Master Chief encounters the alien race known as the Covenant and ancient warriors of the Forerunner empire known as the Prometheans. Master Chief is accompanied by the artificial intelligence construct Cortana."
-		end
-		a + b + c
-	end
-
-	def epilogue
-		a = content_tag :p do
-			"Welcome to Halo Nation, THE best source for all that is Halo! This site offers readers access to an incredible amount of accurate and reliable information on the Halo franchise, ranging from blockbuster game releases to best-selling novels."
-		end
-		b = content_tag :p do
-			"For those who wish to join this epic journey of immersing yourself into this expansive universe, don't miss out on important Wiki and Halo related updates; join the Wiki to track recent changes and news with ease and for free!"
-		end
-		c = content_tag :p do
-			"The game is set exactly four years, seven months and ten days after the events of Halo 3 in 2557, and follows John-117 as he explores the uncharted shield world called Requiem. Here, he discovers new enemies, the return of the Covenant, and more of the Forerunners tainted history."
-		end
-		a + b + c
-	end
-
-	def nightmare
-		a = content_tag :p do
-			"At the age of six, he was identified by Dr. Catherine Halsey as one of 150 preliminary candidates for the SPARTAN-II Program. John was an ideal physical and mental candidate, standing a head taller than the majority of his schoolmates, having greater physical proportions, possessing greater strength, exhibiting superior reflexes, and having an aggressive drive for success."
-		end
-		b = content_tag :p do
-			"When Dr. Halsey and Lieutenant Jacob Keyes visited Eridanus II in 2517 to study him, they were impressed with his intellect and luck. In a final test of his viability as a candidate, he was asked to determine which side an old coin would land on."
-		end
-		c = content_tag :p do
-			"He watched the coin as it flew in the air and caught it before it could land, correctly stating which side was face-up, which was the side of the eagle. John was the first candidate Dr. Halsey and Lieutenant Keyes studied."
-		end
-		a + b + c
-	end
-
-	def aftermath
-		a = content_tag :p do
-			"Four years after the events of Halo 3, a mysterious energy passes through the back section of the UNSC Forward Unto Dawn. Cortana activates the Master Chief's cryopod as an unknown assailant boards the ship. After progressing through part of the ship the assailant turns out to be the Covenant, to which Cortana expresses concern as she believed that humanity had a cease fire with the Sangheili."
-		end
-		b = content_tag :p do
-			"As the Master Chief fights his way through the Covenant boarders, it is revealed that it isn't one rogue covenant ship on a salvage operation, but an entire fleet orbiting a Forerunner world. Cortana notices the nearest cruiser has it's shields down and suggests firing a tactical nuke at it."
-		end
-		c = content_tag :p do
-			"Immediately after the cruiser is destroyed, a passage into the planet opens and activates a gravity well that begins to pull every ship in. The already weakened Forward Unto Dawn starts to disintegrate as it passed into the planet. While making their way to the escape pods, Cortana starts acting erratically."
+			"During her slow and careful descent, the rocks beneath her feet 
+			betrayed her. As Laurie fell, time stood still. A fall from high 
+			up can end in only a matter of seconds, but to the one who is 
+			experiencing it, the fall can appear to last forever. As eternity 
+			came to an end and Laurie made contact with the ground, all hell 
+			broke loose."
 		end
 		a + b + c
 	end
 
 	def prologue
 		a = content_tag :p do
-			"The Master Chief awakens to find himself inside the planet. Cortana reveals her behavioral problem is due to her going into the first stages of Rampancy. The Master Chief suggests trying to find a ship back to Earth to see if Dr. Halsey, Cortana's creator, can fix her condition."
+			"Novels cannot describe the joy that Laurie's life brings to this 
+			world. Paintings and songs cannot capture the beauty and 
+			compassion she possesses. No force can match the impact Laurie 
+			has made on the lives of those around her. She is the most 
+			wonderful, kind, compassionate, and caring person. For those who 
+			have not come to know her, she is someone who would want to know 
+			you; befriend you, care for you, and be there for you. To all of 
+			us, she is a friend, family, and a guardian angel."
 		end
 		b = content_tag :p do
-			"Eventually they fight their way through Covenant forces to a localized Cartographer. Cortana says that the only intel she can get is that their location is a shield world called Requiem. She also receives a signal from an unknown source that is definitely not Covenant in origin, and suggests looking for the signal."
+			"Laurie has been given the a gift of purpose, to bring life and 
+			joy to others. Her soul is so full of life that those around her 
+			would come to live their lives to the fullest. She brings the 
+			best out in all of us, she is a light when all seems dark, and 
+			she is a very important part of our lives. We need her and now 
+			she needs us more than ever."
+		end
+		a + b
+	end
+
+	def nightmare
+		a = content_tag :p do
+			"The nightmare started one dreadful evening on October 18, 2012. 
+			The Sun was low and the dust was settling as Laurie began to 
+			climb. Midway through her ascension, she realized the summit was 
+			out of reach and retreat was the only option available."
+		end
+		b = content_tag :p do
+			"Fearing greater danger, she turned around and began to climb 
+			down the treacherous slopes of Cliff Chaos. During her slow and 
+			careful descent, the rocks beneath her feet betrayed her. The 
+			mountain side casted her away, as if she was just a piece of 
+			debris. As Laurie fell, time stood still. A fall from high up can 
+			end in only a matter of seconds, but to the one who is 
+			experiencing it, the fall can appear to last forever. As eternity 
+			came to an end and Laurie made contact with the ground, all hell 
+			broke loose."
 		end
 		c = content_tag :p do
-			"They find that the mysterious signal is being bounced off of a tower near the planet's core, Cortana opens a portal as close as possible to the core just as a large number of a new type of foe appears."
+			"Her head smashed into the solid ground and her collar bone 
+			shattered. Laurie's skull was split in half and the blood in her 
+			body was no longer hers. The soil would soon claim her precious 
+			blood as she desperately clung to life. For several hours she 
+			would slip in and out of consciousness, waiting for a glimmer of 
+			hope. After many hours of despair, rescue came from afar. A light 
+			in the sky seemed angelic as rescue came from above in the form 
+			of 2 helicopters. Laurie was airlifted out of that hell and flown 
+			to the Enloe Medical Center in Chico, California. The fall may 
+			have ended but the nightmare had only just begun."
+		end
+		a + b + c
+	end
+
+	def aftermath
+		a = content_tag :p do
+			"As soon as the helicopter landed, Laurie was immediately rushed 
+			into the Intensive-Care Unit. She may have narrowly escaped death, 
+			but her wounds were extensive. With no signs of movement or 
+			response, a full recovery was doubtful. Her skull was broken in 
+			half and the inside of her head was exposed. Her collar bone was 
+			shattered and her body was screaming in pain. Permanent damage to 
+			her nervous system seemed inevitable; other than death, this was 
+			the worst possible outcome."
+		end
+		b = content_tag :p do
+			"Shortly after her admission into the ICU, neurosurgeons made 
+			their way into the room. They began operating on her skull, 
+			desperately doing everything that can be done in order to repair 
+			the destruction of Laurie's head. After countless hours of 
+			surgery, a glimpse of hope surfaced. The operation was successful; 
+			however, Laurie was still in critical condition and would remain 
+			in the ICU for a total of five days. Upon her release from the 
+			ICU, she would spend the next 2 weeks in the hospital under 24 
+			hour supervision and care."
+		end
+		c = content_tag :p do
+			"After being discharged from the Enloe Medical Center, Laurie was 
+			transported all the way from Chico to Santa Clara in an ambulance. 
+			Laurie was immediately admitted into a rehabilitation center upon 
+			arrival where she would undergo excruciating physical therapy in 
+			hopes of restoring her mind, body, and spirit to its former self, 
+			or what was left of it. After a week of intense therapy, she was 
+			released back into the world where her mother gave 24 hours a day 
+			7 days a week in dedication to the care of Laurie."
+		end
+		a + b + c
+	end
+
+	def epilogue
+		a = content_tag :p do
+			"She is currently still in recovery at home. We hope for a full 
+			recovery, but only time will tell. Some things can't be repaired, 
+			some things can. She now needs us more than ever. With our support 
+			we can help her financially, mentally, and emotionally. Not only 
+			is her body broken, her spirit is crushed. Her mind is devastated 
+			and her once abundant life force has now diminished."
+		end
+		b = content_tag :p do
+			"However, it is impossible for Laurie to focus solely on her 
+			recovery. She is now faced with a colossal medical bill. At the 
+			time of the accident, Laurie was without health insurance; 
+			therefore, she is forced to burden the medical expenses in its 
+			entirety. Let us help her endure this burden so that she can focus 
+			primarily on recovering her decimated condition."
+		end
+		c = content_tag :p do
+			"No matter how big or small our donations are, no matter how much 
+			or little support we provide, all our charitable givings will help 
+			Laurie on the road to recovery and bring her back to life. From 
+			the kindness of your heart, please wish Laurie the best and 
+			continue to provide her with your relentless support. Thank you 
+			very much."
 		end
 		a + b + c
 	end
