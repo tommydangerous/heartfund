6d0048f29cb5f559a1c89c090a3cabb990e11834
QuantumVenturess <quantumventuress@gmail.com>
2012-12-07 10:28:15 -0800

    removed image laurie4.jpg; updated short_info and about_info; clicking on name logo takes you to the root path; added comment counter

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -27,9 +27,22 @@ $(document).ready(function() {
 	// Regex comment form
 	$('.commentForm .action input').live('click', function() {
 		var content = $('.commentForm .field textarea').val();
-		if (content.length == 0) {
+		if (content.length == 0 || content.length > 255) {
 			$('.commentForm .field textarea').focus();
 			return false;
 		}
 	})
+	// Comment character counter
+	$('.commentForm textarea').keyup(function() {
+		var count = $(this).val().length;
+		var remaining = 255 - count;
+		var counter = $('.commentCounter');
+		if (remaining < 0) {
+			counter.addClass('red');
+		}
+		else {
+			counter.removeClass('red');
+		}
+		counter.html(remaining);
+	})	
 })
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,7 +5,7 @@ module PagesHelper
 		world. Paintings and songs cannot capture the beauty and 
 		compassion she possesses. No force can match the impact Laurie 
 		has made on the lives of those around her. She is the most 
-		wonderful, kind, compassionate, and caring person.
+		wonderful, kind, and caring person.
 		The nightmare started one dreadful evening on October 18, 2012. 
 		The Sun was low and the dust was settling as Laurie began to 
 		climb. Midway through her ascension, she realized the summit was 
@@ -24,7 +24,7 @@ module PagesHelper
 			world. Paintings and songs cannot capture the beauty and 
 			compassion she possesses. No force can match the impact Laurie 
 			has made on the lives of those around her. She is the most 
-			wonderful, kind, compassionate, and caring person."
+			wonderful, kind, and caring person."
 		end
 		b = content_tag :p do
 			"The nightmare started one dreadful evening on October 18, 2012. 
@@ -49,7 +49,7 @@ module PagesHelper
 			world. Paintings and songs cannot capture the beauty and 
 			compassion she possesses. No force can match the impact Laurie 
 			has made on the lives of those around her. She is the most 
-			wonderful, kind, compassionate, and caring person. For those who 
+			wonderful, kind, and caring person. For those who 
 			have not come to know her, she is someone who would want to know 
 			you; befriend you, care for you, and be there for you. To all of 
 			us, she is a friend, family, and a guardian angel."
@@ -84,10 +84,9 @@ module PagesHelper
 			broke loose."
 		end
 		c = content_tag :p do
-			"Her head smashed into the solid ground and her collar bone 
-			shattered. Laurie's skull was split in half and the blood in her 
-			body was no longer hers. The soil would soon claim her precious 
-			blood as she desperately clung to life. For several hours she 
+			"Her head slammed into the solid ground and her collar bone 
+			shattered. Laurie's skull was split in half. She laid there 
+			helpless, desperately clinging to life. For several hours she 
 			would slip in and out of consciousness, waiting for a glimmer of 
 			hope. After many hours of despair, rescue came from afar. A light 
 			in the sky seemed angelic as rescue came from above in the form 
@@ -102,31 +101,29 @@ module PagesHelper
 		a = content_tag :p do
 			"As soon as the helicopter landed, Laurie was immediately rushed 
 			into the Intensive-Care Unit. She may have narrowly escaped death, 
-			but her wounds were extensive. With no signs of movement or 
-			response, a full recovery was doubtful. Her skull was broken in 
-			half and the inside of her head was exposed. Her collar bone was 
-			shattered and her body was screaming in pain. Permanent damage to 
-			her nervous system seemed inevitable; other than death, this was 
-			the worst possible outcome."
+			but her wounds were extensive. With the injuries she sustained, 
+			a full recovery was doubtful. Her skull was nearly broken in 
+			half. Her collar bone was shattered and her body was screaming in 
+			pain. Permanent damage to her nervous system seemed inevitable; 
+			other than death, this was the worst possible outcome."
 		end
 		b = content_tag :p do
-			"Shortly after her admission into the ICU, neurosurgeons made 
-			their way into the room. They began operating on her skull, 
-			desperately doing everything that can be done in order to repair 
-			the destruction of Laurie's head. After countless hours of 
-			surgery, a glimpse of hope surfaced. The operation was successful; 
-			however, Laurie was still in critical condition and would remain 
-			in the ICU for a total of five days. Upon her release from the 
-			ICU, she would spend the next 2 weeks in the hospital under 24 
-			hour supervision and care."
+			"Shortly after her admission into the ICU, 4 neurosurgeons made 
+			their way into the room. They began operating on her head, 
+			desperately doing everything they can to save her life. 
+			After countless hours of surgery, a glimpse of hope surfaced. 
+			The operation was successful; however, Laurie was still in critical 
+			condition and would remain in the ICU for a total of five days. 
+			Upon her release from the ICU, she would spend the next 2 weeks 
+			in the hospital under 24 hour supervision and care."
 		end
 		c = content_tag :p do
 			"After being discharged from the Enloe Medical Center, Laurie was 
 			transported all the way from Chico to Santa Clara in an ambulance. 
-			Laurie was immediately admitted into a rehabilitation center upon 
-			arrival where she would undergo excruciating physical therapy in 
-			hopes of restoring her mind, body, and spirit to its former self, 
-			or what was left of it. After a week of intense therapy, she was 
+			Laurie was immediately admitted into a rehabilitation center  
+			where she would undergo excruciating physical therapy in 
+			hopes of restoring her mind, body, and spirit to its former self. 
+			After a week of intense therapy, she was 
 			released back into the world where her mother gave 24 hours a day 
 			7 days a week in dedication to the care of Laurie."
 		end
@@ -147,16 +144,15 @@ module PagesHelper
 			recovery. She is now faced with a colossal medical bill. At the 
 			time of the accident, Laurie was without health insurance; 
 			therefore, she is forced to burden the medical expenses in its 
-			entirety. Let us help her endure this burden so that she can focus 
-			primarily on recovering her decimated condition."
+			entirety. Let us help her endure this burden so that she may once 
+			again live."
 		end
 		c = content_tag :p do
 			"No matter how big or small our donations are, no matter how much 
 			or little support we provide, all our charitable givings will help 
 			Laurie on the road to recovery and bring her back to life. From 
-			the kindness of your heart, please wish Laurie the best and 
-			continue to provide her with your relentless support. Thank you 
-			very much."
+			the kindness of your heart, please continue to send your prayers 
+			and provide her with your relentless support. Thank you very much."
 		end
 		a + b + c
 	end
