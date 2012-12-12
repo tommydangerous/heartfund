49610937739731335244c0d858b74de893b561ff
QuantumVenturess <quantumventuress@gmail.com>
2012-12-12 14:55:16 -0800

    added root_url function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,12 @@ module ApplicationHelper
 	def to_html(str)
 		simple_format h(str)
 	end
+
+	def root_url
+		if request.fullpath == '/'
+			request.url
+		else
+			request.url.split(request.fullpath)[0]
+		end
+	end
 end
